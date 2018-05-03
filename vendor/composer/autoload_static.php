3c61b031005c136b066ede513a6699f2270b390b
Jérémy Da Trindade <admin@macbook-pro-de-jeremy.home>
2018-05-03 10:08:51 +0200

    autoload hcodebr classes com Slim Application Error

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,8 +6,18 @@ namespace Composer\Autoload;
 
 class ComposerStaticInitdb72da0442ba54f52e56495e6226962d
 {
-    public static $fallbackDirsPsr4 = array (
-        0 => __DIR__ . '/..' . '/hcodebr/php-classes/src',
+    public static $prefixLengthsPsr4 = array (
+        'H' => 
+        array (
+            'Hcode\\' => 6,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'Hcode\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/hcodebr/php-classes/src',
+        ),
     );
 
     public static $prefixesPsr0 = array (
@@ -41,7 +51,8 @@ class ComposerStaticInitdb72da0442ba54f52e56495e6226962d
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->fallbackDirsPsr4 = ComposerStaticInitdb72da0442ba54f52e56495e6226962d::$fallbackDirsPsr4;
+            $loader->prefixLengthsPsr4 = ComposerStaticInitdb72da0442ba54f52e56495e6226962d::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInitdb72da0442ba54f52e56495e6226962d::$prefixDirsPsr4;
             $loader->prefixesPsr0 = ComposerStaticInitdb72da0442ba54f52e56495e6226962d::$prefixesPsr0;
             $loader->classMap = ComposerStaticInitdb72da0442ba54f52e56495e6226962d::$classMap;
 
